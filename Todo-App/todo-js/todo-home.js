7a22aedf1811e636b664ac686f532a701b7ae158
najimali <najim.ali32@gmail.com>
2020-06-25 13:08:46 +0530

    Delete functionality is added

--- Todo-App/todo-js/todo-home.js
+++ Todo-App/todo-js/todo-home.js
@@ -1,28 +1,29 @@
 let input = prompt('What you like to do ??').trim();
 let todoItems = [];
-while(input!=='quit'){
-//    This will add new items in the list 
-    if(input === 'new'){
+while (input !== 'quit') {
+    //    Add new items in the list 
+    if (input === 'new') {
         let newTodo = prompt('Add the items');
         todoItems.push(newTodo);
         console.log(`${newTodo} is added to the list`);
     }
-    // Show the list items in index form 
-    else if(input==='list'){
-        for(let i = 0;i<todoItems.length;i++)
-        console.log(`${i}: ${todoItems[i]}`);
+    //Show the list items in index form 
+    else if (input === 'list') {
+        for (let i = 0; i < todoItems.length; i++)
+            console.log(`${i}: ${todoItems[i]}`);
     }
-    else if (input==='delete'){
+    //Delete the specific item from the list
+    else if (input === 'delete') {
         let index = Math.round(prompt('Enter the index'));
         let deletedItem = -1;
-        if(index>-1){
+        if (index > -1) {
             deletedItem = todoItems[index];
-            todoItems.splice(index,1);
+            todoItems.splice(index, 1);
             console.log(`${deletedItem} is deleted!!`);
-           
+
         }
-        else  console.log('enter a valid index');
-        
+        else console.log('enter a valid index');
+
     }
     input = prompt('What you like to do ??').trim();
 }
