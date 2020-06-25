a563c501c993b09026f02ca0e30b741c5ee7ca12
najimali <najim.ali32@gmail.com>
2020-06-25 13:29:52 +0530

    correctly formatted

--- Todo-App/todo-js/todo-home.js
+++ Todo-App/todo-js/todo-home.js
@@ -1,30 +1,40 @@
 let input = prompt('What you like to do ??').trim();
 let todoItems = [];
 while (input !== 'quit') {
-    //    Add new items in the list 
+    //Add new items in the list 
     if (input === 'new') {
-        let newTodo = prompt('Add the items');
-        todoItems.push(newTodo);
-        console.log(`${newTodo} is added to the list`);
+        addTodo();
     }
     //Show the list items in index form 
     else if (input === 'list') {
-        for (let i = 0; i < todoItems.length; i++)
-            console.log(`${i}: ${todoItems[i]}`);
+        displayTodo();
     }
     //Delete the specific item from the list
     else if (input === 'delete') {
-        let index = Math.round(prompt('Enter the index'));
-        let deletedItem = -1;
-        if (index > -1) {
-            deletedItem = todoItems[index];
-            todoItems.splice(index, 1);
-            console.log(`${deletedItem} is deleted!!`);
-
-        }
-        else console.log('enter a valid index');
+        deleteTodo();
 
     }
     input = prompt('What you like to do ??').trim();
 }
-console.log('Okay ,You have quit the app');
+console.log('Okay ,You  quit the app');
+function addTodo() {
+    let newTodo = prompt('Add the items');
+    todoItems.push(newTodo);
+    console.log(`${newTodo} is added to the list`);
+}
+function displayTodo() {
+    console.log('*******');
+    todoItems.forEach((item, i) => console.log(`${i}: ${item}`));
+    console.log('*******');
+}
+function deleteTodo() {
+    let index = Math.round(prompt('Enter the index to be deleted'));
+    let deletedItem = -1;
+    if (index > -1 && index < todoItems.length) {
+        deletedItem = todoItems[index];
+        todoItems.splice(index, 1);
+        console.log(`${deletedItem} is deleted!!`);
+
+    }
+    else console.log('enter a valid index');
+}
