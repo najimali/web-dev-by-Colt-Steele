3de12185034dd40bb5c6c40ef2e009220f55a9d1
najimali <najim.ali32@gmail.com>
2020-06-25 13:01:54 +0530

    Todo App is added

---
+++ Todo-App/todo-js/todo-home.js
@@ -0,0 +1,28 @@
+let input = prompt('What you like to do ??').trim();
+let todoItems = [];
+while(input!=='quit'){
+
+    if(input === 'new'){
+        let newTodo = prompt('Add the items');
+        todoItems.push(newTodo);
+        console.log(`${newTodo} is added to the list`);
+    }
+    else if(input==='list'){
+        for(let i = 0;i<todoItems.length;i++)
+        console.log(`${i}: ${todoItems[i]}`);
+    }
+    else if (input==='delete'){
+        let index = Math.round(prompt('Enter the index'));
+        let deletedItem = -1;
+        if(index>-1){
+            deletedItem = todoItems[index];
+            todoItems.splice(index,1);
+            console.log(`${deletedItem} is deleted!!`);
+           
+        }
+        else  console.log('enter a valid index');
+        
+    }
+    input = prompt('What you like to do ??').trim();
+}
+console.log('Okay ,You have quit the app');
---
+++ js/practice-js-file.js
@@ -0,0 +1,4 @@
+let numbers =[1,2,3,4,5,6,7,8,9,10];
+numbers.forEach(color=>{
+    if(color%3===0)console.log(color);
+})
