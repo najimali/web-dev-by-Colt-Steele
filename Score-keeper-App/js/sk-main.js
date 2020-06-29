f624b3bfa336ddf7edc1be43d5e0abae314cf750
najimali <najim.ali32@gmail.com>
2020-06-29 22:39:10 +0530

    Reset Button Style Added

--- Score-keeper-App/js/sk-main.js
+++ Score-keeper-App/js/sk-main.js
@@ -1,42 +1,45 @@
 // Activity
 // score1 & score 2 are updating
-let show_score1 = document.querySelector("#score1");
-let show_score2 = document.querySelector("#score2");
+let show_score1 = document.querySelector("#p1");
+let show_score2 = document.querySelector("#p2");
 let p1_score = 0;
 let p2_score = 0;
 let p1_btn = document.querySelector('#p1-btn');
-let show_max_limit = document.querySelector('#maxscore');
+let winnerScoreDisplay = document.querySelector('#winnner-score');
 // console.log(pl_btn);
 
-let max_score=5;
+let max_score = 5;
 //setting max player limit 
-let input =document.querySelector("#max-player");
-if(input===null){
-
-    max_score=5;}
-else{
-    input.addEventListener('input',function(){
-        // if(max_player!==null)
-        console.log(input.value);
-         max_score=input.value;
-         show_max_limit.innerHTML=`Playing to: ${max_score}`;
-    });
-}
+let input = document.querySelector("#max-player");
+input.addEventListener('change',function(){
+    winnerScoreDisplay.innerHTML=input.value;
+    max_score =input.value;
+    reset();
+});
 
 
 
 p1_btn.addEventListener('click', function () {
-
+    p1_btn.classList.toggle('button-purple');
+    setTimeout(function(){
+        p1_btn.classList.toggle('button-purple');
+    }, 80);
+    
     if (p1_score < max_score && p2_score < max_score) {
         p1_score++;
         show_score1.innerHTML = `${p1_score}`;
     }
     if (p1_score == max_score) { show_score1.style.color = 'green'; }
+    // p1_btn.classList.toggle('button-purple');
 });
 let p2_btn = document.querySelector('#p2-btn');
 // console.log(p2_btn );
 //player two
 p2_btn.addEventListener('click', function () {
+    p2_btn.classList.toggle('button-purple');
+    setTimeout(function(){
+        p2_btn.classList.toggle('button-purple');
+    }, 80);
     if (p1_score < max_score && p2_score < max_score) {
         p2_score++;
         show_score2.innerHTML = `${p2_score}`;
@@ -44,13 +47,22 @@ p2_btn.addEventListener('click', function () {
     if (p2_score == max_score) { show_score2.style.color = 'green'; }
 });
 //reset added
-let reset = document.querySelector('#reset-btn');
-reset.addEventListener('click', function () {
+let resetbtn = document.querySelector('#reset-btn');
+resetbtn.addEventListener('click', function () {
+    reset();
+});
+
+
+function reset(){
     p1_score = 0;
     p2_score = 0;
     show_score1.innerHTML = `${p1_score}`;
     show_score2.innerHTML = `${p2_score}`;
-    show_score1.style.color ='black'
-    show_score2.style.color ='black'
-});
+    show_score1.style.color = 'black'
+    show_score2.style.color = 'black'
+    resetbtn.classList.toggle('button-light-blue');
+    setTimeout(function(){
+        resetbtn.classList.toggle('button-light-blue');
+    }, 100);
+}
 
