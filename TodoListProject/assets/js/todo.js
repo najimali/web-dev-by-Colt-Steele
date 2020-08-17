e2c78c9d55248df43c699629c90638b99c8cb765
najimali <najim.ali32@gmail.com>
2020-08-17 23:41:18 +0530

    3D card added

--- TodoListProject/assets/js/todo.js
+++ TodoListProject/assets/js/todo.js
@@ -1,40 +1,24 @@
-//  Logic to mark the note completed & revert back
-// Check off specific todos by Clicking
-// diff between click  &  on 
-// click() only addes listener existing element  
-// but on() will add listerners for all 
-// potential future Element
-$("ul").on("click", "li", function () {
-    //$(this) is only selecting the current li but not all
-    $(this).toggleClass("completed");
+$("#todolist").on("click","li",function(){
+    
+    $(this).toggleClass("checked");
 });
+$("#todolist").on("click","span",function(event){
 
-//Click on X to delete Todo
-$("ul").on("click", "span", function (event) {
-    //here $(this) represent current span & parent is li
-    //we are fading oout but that doesnt mean it is removing so we used
-    // remove
-    $(this).parent().fadeOut(500, function () {
-        //here $(this) is representing li becuause we have called dot parent method
-
+    $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
-    //when we click on span it bounces back to first li then ul then container then bodu then html
-    //so to stop it we use stop Propogation
     event.stopPropagation();
 
 });
-//Creating List
-$("input[type='text'").keypress(function (event) {
-    // 13 is ascii code for enter 
-    if (event.which == 13) {
-
-        let textTodo = $(this).val();
-        $(this).val("");
-        //appending new li to ul
-    	$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + textTodo + "</li>");
-	}	
+$("input[type='text']").keypress(function(event){
+        if(event.which===13){
+            let input = $(this).val();
+            
+            $(this).val("");
+            $("#todolist").append("<li><span><i class='fa fa-trash'></i></span> " + input + "</li>");
+        }
 });
+
 $(".fa-plus").click(function(){
-    $("input[type='text'").fadeToggle();
+    $("input[type='text']").fadeToggle();
 });
